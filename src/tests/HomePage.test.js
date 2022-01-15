c075613e93f10b6421a7f36103d0cfc2ccd3898b
rolandm99 <manfulmweze99@gmail.com>
2022-01-15 21:25:07 +0100

    Add test

---
+++ src/tests/HomePage.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import Home from '../components/HomePage';
+
+describe('HomePage component test', () => {
+  it('Renders the overview of the weather App', () => {
+    const app = renderer
+      .create(
+        <Provider store={store}>
+          <Home />
+        </Provider>,
+      )
+      .toJSON();
+    expect(app).toMatchSnapshot();
+  });
+});
